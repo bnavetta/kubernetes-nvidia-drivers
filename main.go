4fa749a32654c706d2f93c6e93c6dc9099378009
Ben Navetta <ben.navetta@gmail.com>
2017-10-26 01:09:07 -0400

    don't use hard links at all

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"github.com/NVIDIA/nvidia-docker/src/nvidia"
 	"github.com/NVIDIA/nvidia-docker/src/nvml"
+	"io/ioutil"
 	"os"
 	"path"
 )
 
 const VolumeRoot = "/var/lib/kubernetes-nvidia-drivers"
+
+// Been getting weird hardlink issues
+type CopyStrategy struct{}
+
+func (s CopyStrategy) Clone(src, dst string) error {
+	return nvidia.Copy(src, dst)
+}
 
 func createVolume() error {
 	err := nvml.Init()
@@ -17,7 +25,13 @@ func createVolume() error {
 	}
 	defer nvml.Shutdown()
 
-	vols, err := nvidia.LookupVolumes(VolumeRoot)
+	tmp, err := ioutil.TempDir("", "kubernetes-nvidia-drivers")
+	if err != nil {
+		return fmt.Errorf("unable to create staging directory: %s", err)
+	}
+	defer os.RemoveAll(tmp)
+
+	vols, err := nvidia.LookupVolumes(tmp)
 	if err != nil {
 		return fmt.Errorf("error looking up NVIDIA driver volumes: %s", err)
 	}
@@ -25,17 +39,17 @@ func createVolume() error {
 	driverVol := vols["nvidia_driver"]
 	fmt.Printf("Found volume for driver version %s in %s\n", driverVol.Version, driverVol.Path)
 
-	err = driverVol.Create(nvidia.LinkOrCopyStrategy{})
+	err = driverVol.Create(CopyStrategy{})
 	if err != nil {
 		return fmt.Errorf("unable to extract driver files: %s", err)
 	}
 
 	driverPath := path.Join(driverVol.Path, driverVol.Version)
-	currentPath := path.Join(VolumeRoot, "current")
-	fmt.Printf("Linking %s into %s\n", driverPath, currentPath)
-	err = os.Link(driverPath, currentPath)
+	finalPath := path.Join(VolumeRoot, "drivers")
+	fmt.Printf("Renaming %s to %s\n", driverPath, finalPath)
+	err = os.Rename(driverPath, finalPath)
 	if err != nil {
-		return fmt.Errorf("unable to link current driver version: %s", err)
+		return fmt.Errorf("unable to set current driver version: %s", err)
 	}
 
 	return nil
